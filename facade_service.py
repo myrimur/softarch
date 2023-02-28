495a1de8209d4305988bc507a8914dd7e4034743
myrimur <m.t.sobko@gmail.com>
2023-02-28 21:48:52 +0200

    Make calls to logging and messages parallel

--- facade_service.py
+++ facade_service.py
@@ -1,4 +1,7 @@
 from uuid import uuid4
+from threading import Thread
+import logging
+import time
 
 from messaging.base_service import (
     BaseService,
@@ -9,6 +12,8 @@ from messaging.base_service import (
 
 from grpc import insecure_channel, RpcError
 from messaging.messaging_pb2_grpc import BaseStub
+
+logging.basicConfig(level=logging.INFO)
 
 
 class FacadeService(BaseService):
@@ -25,16 +30,69 @@ class FacadeService(BaseService):
         self._messages_service = BaseStub(self._messages_channel)
 
     def _get(self, request: Empty) -> GetResponse:
-        logging = self._logging_service.get(Empty())
-        messages = self._messages_service.get(Empty())
-        return GetResponse(messages=f'logging_service: {logging.messages}; messages_service: {messages.messages}')
+        logging_response = GetResponse()
+        messages_response = GetResponse()
+
+        logging_thread = Thread(target=self._post_to_logging, args=(logging_response,))
+        messages_thread = Thread(target=self._post_to_messages, args=(messages_response,))
+
+        logging_thread.start()
+        messages_thread.start()
+
+        logging_thread.join()
+        messages_thread.join()
+
+        return GetResponse(messages=f'logging_service: {logging_response.messages};'
+                                    f' messages_service: {messages_response.messages}')
+
+    def _get_from_logging(self, logging_response: GetResponse) -> GetResponse:
+        while True:
+            try:
+                logging_response = self._logging_service.get(Empty())
+                return
+            except RpcError:
+                logging.error('Failed to get from logging service, retrying...')
+                time.sleep(1)
+
+    def _get_from_messages(self, messages_response: GetResponse) -> GetResponse:
+        while True:
+            try:
+                messages_response = self._messages_service.get(Empty())
+                return
+            except RpcError:
+                logging.error('Failed to get from messages service, retrying...')
+                time.sleep(1)
 
     def _post(self, request: PostRequest) -> Empty:
         request.uuid = str(uuid4())
-        # TODO: add retrying when failed
-        self._logging_service.post(request)
-        self._messages_service.post(request)
+        logging_thread = Thread(target=self._post_to_logging, args=(request,))
+        messages_thread = Thread(target=self._post_to_messages, args=(request,))
+
+        logging_thread.start()
+        messages_thread.start()
+
+        logging_thread.join()
+        messages_thread.join()
+
         return Empty()
+
+    def _post_to_logging(self, request: PostRequest) -> None:
+        while True:
+            try:
+                self._logging_service.post(request)
+                return
+            except RpcError:
+                logging.error('Failed to post to logging service, retrying...')
+                time.sleep(1)
+
+    def _post_to_messages(self, request: PostRequest) -> None:
+        while True:
+            try:
+                self._messages_service.post(request)
+                return
+            except RpcError:
+                logging.error('Failed to post to messages service, retrying...')
+                time.sleep(1)
 
     def __del__(self):
         self._logging_channel.close()
